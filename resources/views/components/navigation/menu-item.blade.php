0c08e34ae5409695c426c2fb79dba1035ada66b4
ibnsultan <abdulbasitsultan4@gmail.com>
2025-04-27 17:06:05 +0300

    add: create group menu and menu item components for navigation

---
+++ resources/views/components/navigation/menu-item.blade.php
@@ -0,0 +1,8 @@
+<li class="pc-item">
+    <a href="{{ $href }}" class="pc-link">
+        <span class="pc-micon">
+            <i class="{{ $icon }}"></i>
+        </span>
+        <span class="pc-mtext">{{ $slot }}</span>
+    </a>
+</li>
