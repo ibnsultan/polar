c69c864e219c6454fb4fafac0e7ac0d0220ce897
ibnsultan <abdulbasitsultan4@gmail.com>
2025-08-17 17:41:22 +0300

    feat: add offcanvas and modal components for dialog management

---
+++ resources/views/components/dialog/canvas.blade.php
@@ -0,0 +1,3 @@
+<div class="offcanvas {{ $class ?? '' }} {{ $layout ?? 'offcanvas-end' }}" tcabindex="-1" id="{{ $id }}" aria-labelledby="{{ $id }}Label">
+    {{ $slot }}
+</div>
---
+++ resources/views/components/dialog/modal.blade.php
@@ -0,0 +1,5 @@
+<div id="{{ $id }}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="{{$id}}Label" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        {{ $slot }}
+    </div>
+</div>
