05297520c7770d23f99bfb395749885e6e0306cd
ibnsultan <abdulbasitsultan4@gmail.com>
2025-09-02 18:12:55 +0300

    feat: add announcements routes to admin and app route files

--- routes/admin.php
+++ routes/admin.php
@@ -5,8 +5,10 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ModulesController;
 use App\Http\Controllers\Admin\RolesController;
 use App\Http\Controllers\Admin\PermissionsController;
+use App\Http\Controllers\Admin\AnnouncementsController;
 
 Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
 Route::prefix('modules')->group(fn() => ModulesController::routes());
 Route::prefix('roles')->group(fn() => RolesController::routes());
 Route::prefix('permissions')->group(fn() => PermissionsController::routes());
+Route::prefix('announcements')->group(fn() => AnnouncementsController::routes());
--- routes/app.php
+++ routes/app.php
@@ -1,3 +1,17 @@
 <?php
 
-// all routes in here will prefixed to /app/ and will require user to be logged in
+/**
+ * Application Routes
+ *
+ * All Routes defined here will be prefixed to /app/ and will
+ * require user to be logged in
+ * 
+ */
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Base\NotificationController;
+use App\Http\Controllers\Base\AnnouncementController;
+
+Route::group(['prefix' => 'announcements'], fn() => AnnouncementController::routes());
+Route::group(['prefix' => 'notifications', 'as' => 'notifications.'], fn() => NotificationController::routes());
