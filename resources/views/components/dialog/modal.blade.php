42265d7e16e7d7765f7ae91e1aefd89937e1706d
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2025-09-09 19:26:47 +0300

    add: title and header to modal component

--- resources/views/components/dialog/modal.blade.php
+++ resources/views/components/dialog/modal.blade.php
@@ -1,5 +1,18 @@
+@props([ 'title' => null, 'id' => 'modal' ])
+
 <div id="{{ $id }}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="{{$id}}Label" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
-        {{ $slot }}
+        <div class="modal-content">
+            @if($title)
+                <div class="modal-header">
+                    <h5 class="font-semibold" id="{{$id}}Label">{{ $title }}</h5>
+                    <button type="button" class="btn-close" data-pc-modal-dismiss="{{$id}}" aria-label="Close">
+                        <i class="fas fa-times"></i>
+                    </button>
+                </div>
+            @endif
+
+            <div class="modal-body">{{ $slot }}</div>
+        </div>
     </div>
 </div>
