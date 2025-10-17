5dcc65468ea0c33ab5c52bdd5837eef0e3cbe8f5
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2025-10-17 22:31:10 +0300

    refactor: update module action files to match modal component

--- resources/views/admin/modules/partials/add.blade.php
+++ resources/views/admin/modules/partials/add.blade.php
@@ -1,25 +1,17 @@
-<x-dialog.modal id="addModuleModal">
-    <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title">@lang('Add New Module')</h5>
-            <button type="button" class="btn-close" data-pc-dismiss="modal"></button>
+<x-dialog.modal id="addModuleModal" :title="__('Add New Module')">
+    <form action="{{ route('admin.modules.store') }}" method="POST" onsubmit="submitForm(event)">
+        @csrf
+        <div class="mb-3">
+            <label for="add_name" class="form-label">@lang('Module Name') <span class="text-danger">*</span></label>
+            <input type="text" class="form-control" id="add_name" name="name" required placeholder="@lang('Enter module name')">
         </div>
-        <div class="modal-body">
-            <form action="{{ route('admin.modules.store') }}" method="POST" onsubmit="submitForm(event)">
-                @csrf
-                <div class="mb-3">
-                    <label for="add_name" class="form-label">@lang('Module Name') <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" id="add_name" name="name" required placeholder="@lang('Enter module name')">
-                </div>
-                <div class="mb-3">
-                    <label for="add_description" class="form-label">@lang('Description') <span class="text-danger">*</span></label>
-                    <textarea class="form-control" id="add_description" name="description" rows="3" required placeholder="@lang('Enter module description')"></textarea>
-                </div>
-                <div class="text-end">
-                    <button type="button" class="btn btn-secondary" data-pc-dismiss="modal">@lang('Cancel')</button>
-                    <button type="submit" class="btn btn-primary">@lang('Create Module')</button>
-                </div>
-            </form>
+        <div class="mb-3">
+            <label for="add_description" class="form-label">@lang('Description') <span class="text-danger">*</span></label>
+            <textarea class="form-control" id="add_description" name="description" rows="3" required placeholder="@lang('Enter module description')"></textarea>
         </div>
-    </div>
+        <div class="text-end">
+            <button type="button" class="btn btn-secondary" data-pc-dismiss="modal">@lang('Cancel')</button>
+            <button type="submit" class="btn btn-primary">@lang('Create Module')</button>
+        </div>
+    </form>
 </x-dialog.modal>
--- resources/views/admin/modules/partials/edit.blade.php
+++ resources/views/admin/modules/partials/edit.blade.php
@@ -1,26 +1,18 @@
-<x-dialog.modal id="editModuleModal">
-    <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title">@lang('Edit Module')</h5>
-            <button type="button" class="btn-close" data-pc-dismiss="modal"></button>
+<x-dialog.modal id="editModuleModal" :title="__('Edit Module')">
+    <form action="" method="POST" onsubmit="submitForm(event)" id="editModuleForm">
+        @csrf
+        @method('PUT')
+        <div class="mb-3">
+            <label for="edit_name" class="form-label">@lang('Module Name') <span class="text-danger">*</span></label>
+            <input type="text" class="form-control" id="edit_name" name="name" required>
         </div>
-        <div class="modal-body">
-            <form action="" method="POST" onsubmit="submitForm(event)" id="editModuleForm">
-                @csrf
-                @method('PUT')
-                <div class="mb-3">
-                    <label for="edit_name" class="form-label">@lang('Module Name') <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" id="edit_name" name="name" required>
-                </div>
-                <div class="mb-3">
-                    <label for="edit_description" class="form-label">@lang('Description') <span class="text-danger">*</span></label>
-                    <textarea class="form-control" id="edit_description" name="description" rows="3" required></textarea>
-                </div>
-                <div class="text-end">
-                    <button type="button" class="btn btn-secondary" data-pc-dismiss="modal">@lang('Cancel')</button>
-                    <button type="submit" class="btn btn-primary">@lang('Update Module')</button>
-                </div>
-            </form>
+        <div class="mb-3">
+            <label for="edit_description" class="form-label">@lang('Description') <span class="text-danger">*</span></label>
+            <textarea class="form-control" id="edit_description" name="description" rows="3" required></textarea>
         </div>
-    </div>
+        <div class="text-end">
+            <button type="button" class="btn btn-secondary" data-pc-dismiss="modal">@lang('Cancel')</button>
+            <button type="submit" class="btn btn-primary">@lang('Update Module')</button>
+        </div>
+    </form>
 </x-dialog.modal>
